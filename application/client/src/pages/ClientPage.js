be519d9ce1ab70336311901cf0b450e1e087d323
Vlad-Pischaeff <pischaeff@gmail.com>
2021-08-20 12:53:21 +0500

    updates ...

--- application/client/src/pages/ClientPage.js
+++ application/client/src/pages/ClientPage.js
@@ -1,5 +1,6 @@
 import { useAuth } from '../hooks/auth.hook'
 import { $URL } from '../service/Service'
+import MessageList from '../components/MessageList'
 import '../sass/ClientPage.sass'
 
 export default function ClientPage() {
@@ -8,10 +9,13 @@ export default function ClientPage() {
   return (
     <section className="cp">
       <div className="cp_header">
-
+        <picture className="cp_header-avatar">
+          <img className="cp_header-avatarimg" src={`${$URL}/img/app/profile2.png`} alt='' />
+          <div className="cp_header-avatarbg"></div>
+        </picture>
       </div>
       <div className="cp_body">
-
+        <MessageList />
       </div>
       <div className="cp_footer">
         <div className="chat_input">
