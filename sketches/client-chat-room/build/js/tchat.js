e2900557ecb01087a5c6e5b6db32f04926ff658a
Vlad-Pischaeff <pischaeff@gmail.com>
2021-08-31 14:47:12 +0500

    update ...

---
+++ sketches/client-chat-room/build/js/tchat.js
@@ -0,0 +1,23 @@
+let tchat = document.getElementById('tchat_wrap')
+let button = document.getElementById('tchat_button')
+let button_close = document.getElementById('tchat_close')
+
+button.addEventListener('click', () => {
+  tchat.classList.add('bounceIn')
+  tchat.classList.remove('flipOutY')
+  button_close.style.display = 'inherit'
+  button.style.display = 'none'
+  button_close.blur()
+  if (document.activeElement instanceof HTMLElement)
+    document.activeElement.blur()
+})
+
+button_close.addEventListener('click', () => {
+  tchat.classList.remove('bounceIn')
+  tchat.classList.add('flipOutY')
+  button_close.style.display = 'none'
+  button.style.display = 'inherit'
+  button.blur()
+  if (document.activeElement instanceof HTMLElement)
+    document.activeElement.blur()
+})
