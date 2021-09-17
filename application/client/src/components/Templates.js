f89eaa79c1dbc355f5867d6c97abc89eedfb14d6
Vlad-Pischaeff <pischaeff@gmail.com>
2021-09-17 12:56:06 +0500

    updates ...

--- application/client/src/components/Templates.js
+++ application/client/src/components/Templates.js
@@ -1,4 +1,6 @@
 import { Link, useLocation } from "react-router-dom"
+import TooltipWrap from "./TooltipWrap"
+import { $URL, $G } from "../service/Service"
 
 export default function Templates() {
   const location  = useLocation()
@@ -7,8 +9,26 @@ export default function Templates() {
 
   return (
     <div className="templates">
-      <Link to={{ pathname: "/modaladdr", state: { background: location }}}>Modal Page</Link>
-      <a href="http://localhost:5000/tchat">Client Page</a>
+      <div className="templates_header">
+        <TooltipWrap position="tip-right" tip="Quick short answers...">
+          <img className="chat_input-icon" src={`${$URL}/img/app/note.png`} alt='Answers'/>
+        </TooltipWrap>
+      </div>
+      <div className="templates_body">
+      {
+        $G.ACC.answer.map((n, i) => { 
+          return (
+            <div className="forms_field" key={i} onClick={() => {}} onKeyUp={() => {}}>
+              <p>{n}</p>
+            </div>
+          )
+        })
+      }
+      </div>
+      <div className="templates_footer">
+        <Link to={{ pathname: "/modaladdr", state: { background: location }}}>Modal Page</Link>
+        <a href="http://localhost:5000/tchat">Client Page</a>
+      </div>
     </div>
   )
 }
