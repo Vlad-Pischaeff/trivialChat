e30f504c2dc6e7dab87eb20a23883d8d173a1d67
Vlad-Pischaeff <pischaeff@gmail.com>
2021-10-22 08:57:14 +0500

    switch onloadend to onload for Chrome support...

--- application/client/src/js/__ResizeImage.js
+++ application/client/src/js/__ResizeImage.js
@@ -1,38 +1,35 @@
-export default function __ResizeImage(datas){
-  return new Promise(async function(resolve,reject){
-      let wantedWidth, wantedHeight, maxDim = 400
-      // We create an image to receive the Data URI
-      var img = document.createElement('img');
+export default function __ResizeImage(datas) {
+  return new Promise(async function(resolve,reject) {
+    let wantedWidth, wantedHeight, maxDim = 500
+    // We create an image to receive the Data URI
+    var img = document.createElement('img')
+    // When the event "onload" is triggered we can resize the image.
+    img.onload = function() {        
+      // We create a canvas and get its context.
+      var canvas = document.createElement('canvas')
+      var ctx = canvas.getContext('2d')
 
-      // When the event "onload" is triggered we can resize the image.
-      img.onloadend = function()
-      {        
-          // We create a canvas and get its context.
-          var canvas = document.createElement('canvas');
-          var ctx = canvas.getContext('2d');
+      if (img.width > img.height) {
+        wantedWidth = maxDim
+        wantedHeight = (img.height / img.width) * maxDim
+      } else {
+        wantedHeight = maxDim
+        wantedWidth = (img.width / img.height) * maxDim
+      }
+      // We set the dimensions at the wanted size.
+      canvas.width = wantedWidth
+      canvas.height = wantedHeight
+      console.log('wanted...', wantedWidth, wantedHeight, img.width, img.height)
+      // We resize the image with the canvas method drawImage();
+      ctx.drawImage(this, 0, 0, wantedWidth, wantedHeight)
 
-          if (img.width > img.height) {
-            wantedWidth = maxDim
-            wantedHeight = (img.height / img.width) * maxDim
-          } else {
-            wantedHeight = maxDim
-            wantedWidth = (img.width / img.height) * maxDim
-          }
-          // We set the dimensions at the wanted size.
-          canvas.width = wantedWidth;
-          canvas.height = wantedHeight;
-          console.log('wanted...', wantedWidth, wantedHeight, img.width, img.height)
-          // We resize the image with the canvas method drawImage();
-          ctx.drawImage(this, 0, 0, wantedWidth, wantedHeight);
+      var dataURI = canvas.toDataURL()
 
-          var dataURI = canvas.toDataURL();
-
-          // This is the return of the Promise
-          resolve(dataURI);
-      };
-
-      // We put the Data URI in the image's src attribute
-      img.src = datas;
-
+      // This is the return of the Promise
+      resolve(dataURI)
+    }
+    // We put the Data URI in the image's src attribute
+    img.src = datas
   })
-}// Use it like : var newDataURI = await resizedataURL('yourDataURIHere', 50, 50);
+}
+// Use it like : var newDataURI = await resizedataURL('yourDataURIHere', 50, 50);
